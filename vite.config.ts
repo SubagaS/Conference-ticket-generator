96b205ac460177354d63e1d98d51f255066ed942
Subaga <subaga98@gmail.com>
2025-10-02 12:52:51 +0200

    Update the dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Frontend-mentor/Junior/Conference-ticket-generator',
+  base: '/Frontend-mentor/Junior/Conference-ticket-generator/',
 });
